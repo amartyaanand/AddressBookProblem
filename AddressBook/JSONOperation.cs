42c957957e7f3106d05d2554da248344d68fb935
Amartya Anand <amartyaanand21@live.com>
2020-12-20 12:44:19 +0530

    [Amartya] Add : Read and Write to JSON file.

---
+++ AddressBook/JSONOperation.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AddressBook
+{
+    class JSONOperation
+    {
+        string filePath = @"D:\GitBash\AddressBookProblem\AddressBook\Utility\AddressBookRecord.json";
+        public void WriteToFile(Dictionary<string, AddressBook> addressBookDictionary)
+        {
+            string json = "";
+            foreach (AddressBook obj in addressBookDictionary.Values)
+            {
+                foreach (Contact contact in obj.addressBook.Values)
+                {
+                    json = JsonConvert.SerializeObject(contact);
+                    File.WriteAllText(filePath, json);
+                }
+            }
+            Console.WriteLine("\nSuccessfully added to JSON file.");
+        }
+        public void ReadFromFile()
+        {
+            Console.WriteLine("Below are Contents of JSON File");
+            var json = File.ReadAllText(filePath);
+            Contact contact = JsonConvert.DeserializeObject<Contact>(json);
+            Console.WriteLine(contact.ToString());
+        }
+    }
+}
